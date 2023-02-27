d1110b85294e523e68234b42ad93ed6d669f0526
Nahom32 <senaynahom00@gmail.com>
2023-02-27 08:41:47 +0300

    Initial commit for asp .net

---
+++ learningProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace learningProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
