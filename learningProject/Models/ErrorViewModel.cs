b6759b9f5f30b80f763437db198f7e50ca739237
Nahom32 <senaynahom00@gmail.com>
2023-02-27 09:51:41 +0300

    Changed to .net 7

--- learningProject/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace learningProject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
